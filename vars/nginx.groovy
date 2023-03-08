1cd6fa6751620ab8970709c6b26d19c5af009043
JanardhanReddy <bommireddy246@gmail.com>
2023-03-08 16:32:47 +0000

    pipeline update shared library

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -2,13 +2,11 @@ def call() {
   node() {
 
     common.pipelineinit()
-
     common.codeChecks()
 
     if( env.BRANCH_NAME == env.TAG_NAME )
     {
       common.publishArtifacts()
-
     }
 
   }
