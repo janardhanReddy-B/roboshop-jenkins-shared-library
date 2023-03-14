769d760cdb4e8a26e586471dd5a9c91f613d2028
JanardhanReddy <bommireddy246@gmail.com>
2023-03-14 23:10:23 +0000

    pipeline update shared library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,16 +1,17 @@
 def call() {
   node () {
-    stage ('Terraform INIT') {
-      sh "terraform init"
-    }
+    ansiColor('xterm') {
+      stage('Terraform INIT') {
+        sh "terraform init"
+      }
 
-    stage('Terrafrom plan'){
-      sh "terraform plan"
-    }
+      stage('Terrafrom plan') {
+        sh "terraform plan"
+      }
 
-    stage('Terrafrom Apply'){
-      sh "terraform apply -auto-approve"
+      stage('Terrafrom Apply') {
+        sh "terraform apply -auto-approve"
+      }
     }
-
   }
 }
