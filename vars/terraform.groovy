fb348fe2be6df8f4d94f1045608b7ea5b180aa53
JanardhanReddy <bommireddy246@gmail.com>
2023-03-15 02:13:07 +0000

    pipeline update shared library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -10,8 +10,8 @@ def call() {
     ansiColor('xterm') {
 
       stage('Code Checkout') {
-        //sh 'find . | sed -e "1d" | xargs rm -rf'
-        git branch: 'main', url: "https://github.com/janardhanReddy-B/${REPO_NAME}.git"
+        sh 'find . | sed -e "1d" | xargs rm -rf'
+        git branch: 'main', url: "https://github.com/janardhanReddy-B/roboshop-terraform-mutable.git"
       }
 
       stage('Terraform INIT') {
